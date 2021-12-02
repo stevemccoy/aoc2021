880579de8b9e1c33833053e12b3bdb755f97fc42
Steve McCoy <stephen.mccoy@bjss.com>
2021-12-02 08:42:09 +0000

    Day 2, Part 1

--- clojure/aoc2021/src/aoc2021/core.clj
+++ clojure/aoc2021/src/aoc2021/core.clj
@@ -1,5 +1,6 @@
 (ns aoc2021.core)
 (require '[clojure.java.io :as io])
+(require '[clojure.string :as str])
 
 (defn count-increases
   "Count the number of depth samples which are increases from the previous sample"
@@ -25,3 +26,30 @@
     (println "Number of 3-window sum increases = "
              (count-increases (window3-sums (map #(Integer/parseInt %) (line-seq rdr))))
              )))
+
+(defn sum-by-verb [instructions verb]
+  (->> instructions
+       (filter #(= (first %) verb))
+       (map #(second %))
+       (reduce +)))
+
+(defn day2-part1 []
+  (with-open [rdr (io/reader "input/day2.txt")]
+    (let [raw (map #(str/split % #" ") (line-seq rdr))]
+      (let [parsed (map #(list (first %) (Integer/parseInt (second %))) raw)]
+        (let [horizontal (sum-by-verb parsed "forward")
+              vertical (- (sum-by-verb parsed "down")
+                          (sum-by-verb parsed "up"))
+              product (* horizontal vertical)]
+          (doall ((println "Day2, Part 1. Ending Position:" )
+                  (println "Horizontal = " horizontal)
+                  (println "Vertical = " vertical)
+                  (println "Product = " product)
+                  product))
+          )
+        )
+      )
+    )
+  )
+
+
