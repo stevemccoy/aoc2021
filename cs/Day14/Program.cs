a3cc056a9ac65a95cc3b86f137397f2a3d359a27
Steve McCoy <stephen.mccoy@bjss.com>
2022-01-10 09:14:16 +0000

    Day 14, part 2, new approach (WIP)

--- cs/Day14/Program.cs
+++ cs/Day14/Program.cs
@@ -6,10 +6,27 @@ using System.Text;
 
 namespace Day14
 {
+    class Polymer
+    {
+        public int Steps { get; set; }
+        public string Strand { get; set; }
+
+        public Polymer(int steps, string strand)
+        {
+            Steps = steps;
+            Strand = strand;
+        }
+    }
+
     class Program
     {
+        const int StringSplitLength = 1024;
+        const int NumStringSplits = 8;
+
         static Dictionary<string, string> Rules = new Dictionary<string, string>();
         static string Strand = "";
+        static Stack<Polymer> Work = new Stack<Polymer>();
+        static Dictionary<char, Int64> RunningStats = new Dictionary<char, Int64>();
 
         static void Main(string[] args)
         {
@@ -31,19 +48,75 @@ namespace Day14
             Console.WriteLine($"Difference = {maxValue - minValue}");
 
             Console.WriteLine("Part 2.");
-            DoSteps(30);
-            stats = Statistics().OrderBy(p => p.Value);
+            Part2();
+            stats = RunningStats.OrderBy(p => p.Value);
             minValue = stats.First().Value;
             maxValue = stats.Last().Value;
             Console.WriteLine($"Most common: {stats.Last().Key} : {maxValue}");
             Console.WriteLine($"Least common: {stats.First().Key} : {minValue}");
             Console.WriteLine($"Difference = {maxValue - minValue}");
-
         }
 
-        private static void DoDemo(string input)
+        static void Part2() {
+            ReadInputFile("input14.txt");
+            SetupStatistics();
+            Work.Push(new Polymer(0, Strand));
+            ProcessDay2Work(40);
+        }
+
+        private static void ProcessDay2Work(int numSteps)
         {
-            Strand = input;
+            Polymer p;
+            char? previousChar = null;
+            while (Work.TryPop(out p))
+            {
+                bool deferStats = false;
+                // Grab strand and process.
+                while (p.Steps < numSteps)
+                {
+                    p.Strand = DoStep(p.Strand, previousChar);
+                    p.Steps++;
+                    if (p.Strand.Length > StringSplitLength) {
+                        SplitAndPushWork(p);
+                        deferStats = true;
+                        break;
+                    }
+                }
+                if (!deferStats) {
+                    AddStringToStats(p.Strand);
+                    previousChar = p.Strand.Last<char>();
+                }
+            }
+        }
+
+        private static void SplitAndPushWork(Polymer p)
+        {
+            var input = p.Strand;
+            int n = StringSplitLength / NumStringSplits;
+            while (input.Length > n) {
+                Work.Push(new Polymer(p.Steps, input.Substring(input.Length - n)));
+                input = input.Substring(0, input.Length - n);
+            }
+            if (input.Length > 0) {
+                Work.Push(new Polymer(p.Steps, input));
+            }
+        }
+
+        private static void SetupStatistics()
+        {
+            RunningStats.Clear();
+        }
+
+        private static void AddStringToStats(string strand) 
+        {
+            foreach (char ch in strand) {
+                if (RunningStats.ContainsKey(ch)) {
+                    RunningStats[ch]++;
+                }
+                else {
+                    RunningStats[ch] = 1;
+                }
+            }
         }
 
         static void ReadInputFile(string fileName)
@@ -67,10 +140,18 @@ namespace Day14
                 Strand = DoStep(Strand);
             }
         }
-        static string DoStep(string input) {
+        static string DoStep(string input, char? previousChar = null) {
             input += " ";
-            StringBuilder output = new StringBuilder("", input.Length * 2);
             string from = "";
+            StringBuilder output = new StringBuilder("", input.Length * 2);
+            // Deal with replacement due to previous character if needed.
+            if (previousChar.HasValue) {
+                from = previousChar.Value + input[0].ToString();
+                if (Rules.ContainsKey(from)) {
+                    output.Append(Rules[from]);
+                }
+            }
+            // Process the main part of the input string.
             for (int i = 0; i < input.Length - 1; i++) {
                 from = input.Substring(i, 2);
                 output.Append(from[0]);
@@ -78,7 +159,6 @@ namespace Day14
                     output.Append(Rules[from]);
                 }
             }
-//            output.Append(input[input.Length - 2]);
             return output.ToString();
         }
 
